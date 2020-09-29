947b28dd31d2def6d49f77f265f781c61d258573
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-09-29 00:52:51 -0500

    (0x0C) task 11

--- 0x0C-javascript_advanced/11-prime.js
+++ 0x0C-javascript_advanced/11-prime.js
@@ -12,8 +12,10 @@ function countPrimeNumbers () {
 }
 
 const t0 = performance.now();
-for (let i = 0; i < 100; i++) {
-  countPrimeNumbers();
-}
+setTimeout(function () {
+  for (let i = 0; i < 100; i++) {
+    countPrimeNumbers();
+  }
+}, 0)
 const t1 = performance.now();
 console.log(`Execution time of calculating prime numbers 100 times was ${t1 - t0} milliseconds`)
